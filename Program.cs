85832c4d64fe09b44168a7247d5de90bb69b861e
Matheus Mutti <matheusmuttiteixeira@gmail.com>
2024-01-05 13:58:07 -0300

    Desafio realizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nokia");
+        Nokia nokia = new Nokia("1111111111", "modelo padrao", "223232323", 64);
+        nokia.InstalarAplicativo("WhatsApp");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+
+        Console.WriteLine("\n");
+
+
+        Console.WriteLine("Iphone");
+        Iphone iphone = new Iphone("222222222222", "modelo padrao", "454545454", 128);
+        iphone.InstalarAplicativo("Telegram");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+    }
+}
